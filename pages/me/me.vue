0f58fc0a0a651dab7a8f379345a9e94841bc00ac
gty <513884454@qq.com>
2020-06-08 15:52:37 +0800

    更换为微信登录模板，添加.gitnore文件，需要调整界面颜色

---
+++ pages/me/me.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="content">
+		<view v-if="hasLogin" class="hello">
+			<view class="title">
+				您好 {{userName}}，您已成功登录。
+			</view>
+		</view>
+		<view v-if="!hasLogin" class="hello">
+			<view class="btn-row">
+				<button type="primary" class="primary" @tap="toLogin">账号登录</button>
+			</view>
+		</view>
+		
+	</view>
+	
+</template>
+
+<script>
+	import {
+		mapState
+	} from 'vuex'
+	export default {
+		computed:mapState(['forcedLogin', 'hasLogin', 'userName','studentId']),
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			toLogin(){
+				uni.navigateTo({
+					url: '../login/login'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
