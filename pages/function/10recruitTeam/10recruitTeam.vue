0ebede70df0499df5a3fa1617fb79371240a4bca
lsq6477 <liusiqi4777@gmail.com>
2020-06-14 20:44:44 +0800

    完成main

---
+++ pages/function/10recruitTeam/10recruitTeam.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		招生团队申请
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/function/14sponGovVerify/14sponGovVerify.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		公派申请审核
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
