8885c3e4875501f03eabaeda6d868e9e99fc44cc
lsq6477 <liusiqi4777@gmail.com>
2020-06-16 19:47:21 +0800

    完成 教师个人信息维护 界面

--- pages/function/2studentInfo/2studentInfo.vue
+++ pages/function/2studentInfo/2studentInfo.vue
@@ -1,11 +1,29 @@
-<template>
-	<view>
-		学生信息查看
+-<template>
+	<view class="content">
+		<view v-if="hasLogin" class="hello">
+			<view class="title">
+				学生信息查看
+			</view>
+			
+		</view>
+		<view v-if="!hasLogin" class="hello">
+			<view class="title">
+				您好 游客。
+			</view>
+			<view class="ul">
+				<view>这是 uni-app 带登录模板的示例App首页。</view>
+				<view>在 “我的” 中点击 “登录” 可以 “登录您的账户”</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import {
+		mapState
+	} from 'vuex'
 	export default {
+		computed: mapState(['hasLogin', 'teacherId']),
 		data() {
 			return {
 				
@@ -18,5 +36,10 @@
 </script>
 
 <style>
-
+	.title {
+			font-size: 40rpx;
+			background-color: #DDDDDD;
+			text-align: center;
+			height: 60rpx;
+		}
 </style>
